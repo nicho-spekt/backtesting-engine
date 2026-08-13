b85ad6cce3f35e71ab84f594f200efe34dba22d8
SpektorNicholas <nicholas.spekt@gmail.com>
2026-08-13 14:59:44 -0400

    fix: added preprocessors

--- src/cpp/include/MetricsEngine.h
+++ src/cpp/include/MetricsEngine.h
@@ -1,5 +1,7 @@
 #pragma once
 #include <vector>
+#ifndef METRICSENGINE_H
+#define METRICSENGINE_H
 
 class MetricsEngine{
 
@@ -10,3 +12,5 @@ class MetricsEngine{
     double getMaxDrawdown(const std::vector<double>& portfolioValues);
 
 };
+
+#endif
